59a5deeb2c8db637290cd6e31ffd3f5f8730bb93
Janith Madarasinghe <73998693+Janithpm@users.noreply.github.com>
2023-10-30 11:32:56 +0530

    Revert "Revert "employee delete""
    This reverts commit b92db6e9ddf3cc6e57d343dc2495bf6389f27919.

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -1,7 +1,7 @@
 import nodeMailer from 'nodemailer'
 
 const SMTP_EMAIL = 'janithpm0@gmail.com'
-const SMTP_PASSWORD = 'xsmtpsib-11b13058515c6e101be97a8cf1b62115985f3984c3f81c2826a2c13cb91deb0d-Y1QJUtrFbG3nVKSg'
+const SMTP_PASSWORD = 'NV1I0BghQD2W3x8C'
 // const SMTP_EMAIL = 'janithpm9991@gmail.com'
 // const SMTP_PASSWORD = 'xsmtpsib-f39375e6e710cf7e149d76b75bd2f1d7f126cecd3f10a8c471509c3227941f2d-Iw2dUM0aJVqL8KrW'
 
@@ -24,6 +24,8 @@ const generateMailForSendPassword = (username: string, password: string) => {
     )
 }
 
+
+
 const sendMail = async (to: string, subject: string, html: string) => {
     try {
         const info = await transporter.sendMail({
@@ -33,6 +35,9 @@ const sendMail = async (to: string, subject: string, html: string) => {
             html,
         })
 
+
+
+        console.log(info);
         return info
 
     } catch (err) {
